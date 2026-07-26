90ae032f7a39ca6080f666ae0971ad06e0e966f8
Fopefoluwa Ikufisile <fope.ikufisile@gmail.com>
2026-07-26 01:38:11 -0500

    finalized migrations cript

--- migrate.sh
+++ migrate.sh
@@ -7,14 +7,12 @@ set -a
 getProjectsUrls() {
     projects=$(curl -s "https://sheets.googleapis.com/v4/spreadsheets/$SPREADSHEET_ID/values/$RANGE?key=$API_KEY" | jq -r '.values[0:8] | map(select(.[4] == "Frontend Mentor")) | map(.[2])[]')
 
-    echo $projects
-
-    for project in $projects; do
-        project_name=$(basename $project)
+    echo "$projects" | while IFS= read -r project; do
+        [ -z "$project" ] && continue
+        project_name=$(basename "$project" .git)
         echo "--> Importing $project_name from $project..."
-        git subtree add --prefix=$project_name $project main
+        git subtree add --prefix="$project_name" "$project" main
     done
-
 }
 
 getProjectsUrls
